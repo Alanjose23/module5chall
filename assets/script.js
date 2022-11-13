c841582bae6ca2d9aae2a9f994e14f6dae5502d9
ALAN JOSE <alanjose04231120@gmail.com>
2022-11-12 23:48:18 -0500

    added comments

--- assets/script.js
+++ assets/script.js
@@ -9,10 +9,10 @@ var endTime =  moment("5:00 pm", "hh:mm A");
 console.log(startTime);
 console.log(endTime);
 
-for(var i = 0; i < 1; i++){
-// create and append time blocks
-// append text and create the time blocks
-startTime.format("LLL")
+// for(var i = 0; i < 8; i++){
+// tried to create and append time blocks
+// tried to append text and create the time blocks
+// console.log(startTime.format("LLL"));
+// console.log(startTime.add(1,'hours').format("LLL"));
 
-
-}
+// }
